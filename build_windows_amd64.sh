cfdba2132eaae0f89d74d5e6ac3c09512dad88a3
idroz <idrozdov@beringresearch.com>
2025-01-02 22:59:55 +0000

    add windows build scripts

--- build_windows_amd64.sh
+++ build_windows_amd64.sh
@@ -1,3 +1,3 @@
 CGO_ENABLED=1 GOOS=windows CC="x86_64-w64-mingw32-gcc" GOARCH=amd64 go build -o dist/Mezmer_Win_x64.exe
 
-CGO_ENABLED=1 GOOS=windows CC="i686-w64-mingw32-gcc" GOARCH=386 go build -o dist/Mezmer_Win_x64.exe
+CGO_ENABLED=1 GOOS=windows CC="i686-w64-mingw32-gcc" GOARCH=386 go build -o dist/Mezmer_Win_x32.exe
