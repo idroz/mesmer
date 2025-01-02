2eb1ae16abb7115f7da7b3cb7e862f4673f17d40
idroz <idrozdov@beringresearch.com>
2025-01-02 10:47:11 +0000

    update build scripts

--- build_darwin_arm64.sh
+++ build_darwin_arm64.sh
@@ -14,25 +14,23 @@ go run macappgo/macapp.go \
 cp ./resources/Info.plist ./app/Mezmer.app/Contents
 
 APP_PATH="app/Mezmer.app"
-
 APP_CERTIFICATE="Developer ID Application: Ignat Drozdov (27B2YLEUVR)"
-codesign --timestamp --options=runtime -s "$APP_CERTIFICATE" -v --entitlements ./resources/entitlements.plist ./app/Mezmer.app
+codesign --timestamp --options=runtime -s "$APP_CERTIFICATE" -v --entitlements ./resources/entitlements.plist "$APP_PATH"
 
-ZIP_PATH="Mezmer.zip"
+ZIP_PATH="Mezmer_Darwin_arm64.zip"
 
 # Create a ZIP archive suitable for notarization.
 /usr/bin/ditto -c -k --keepParent "$APP_PATH" "$ZIP_PATH"
 
+xcrun notarytool submit "$ZIP_PATH" --keychain-profile "notarytool-password" --wait
 
-xcrun notarytool submit Mezmer.zip --keychain-profile "notarytool-password" --wait
-
-unzip Mezmer.zip
-rm Mezmer.zip
+unzip $ZIP_PATH
+rm $ZIP_PATH
 xcrun stapler staple "Mezmer.app"
 
 mkdir dist
 mv Mezmer.app dist/
-/usr/bin/ditto -c -k --keepParent "dist/Mezmer.app" "dist/Mezmer.zip"
+/usr/bin/ditto -c -k --keepParent "dist/Mezmer.app" "dist/$ZIP_PATH"
 
 rm -r app bin
 sudo rm -r dist/Mezmer.app
